480d5d8f33d0ff29ee64359bbb8a575dba9325ca
NIKHIL KURMI <nikhil.lu210@gmail.com>
2023-07-29 19:54:20 +0600

    Feature/sport crud (#6)
    * Sports Crud
    
    * Updated
    
    ---------
    
    Co-authored-by: Md Omor Faruk <mdomorfaruk.lu.cse@gmail.com>

---
+++ app/Http/Requests/Administration/Sport/SportStoreRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Administration\Sport;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SportStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'unique:sports,name'],
+            "status" => ['required','in:Active,Inactive'],
+        ];
+    }
+    
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'status.in' => 'The Status should be Active or Inactive only.',
+        ];
+    }
+}
--- resources/views/administration/sport/index.blade.php
+++ resources/views/administration/sport/index.blade.php
@@ -5,105 +5,86 @@
 
 @endsection
 
-@section('page_title', __('Dashboard'))
+@section('page_title', __('All Sports'))
 
 @section('css_links')
     {{--  External CSS  --}}
 @endsection
 
 @section('custom_css')
-    {{--  External CSS  --}}
-    <style>
-    /* Custom CSS Here */
-    </style>
+    <!-- DataTables css -->
+    <link href="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Responsive Datatable css -->
+    <link href="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 
+@section('custom_css')
+{{--  External CSS  --}}
+<style>
+  /* Custom CSS Here */
+</style>
+@endsection
 
 @section('page_name')
-    <b class="text-uppercase">{{ __('Dashboard') }}</b>
+    <b class="text-uppercase">{{ __('All Sports') }}</b>
 @endsection
 
 
 @section('breadcrumb')
-    <li class="breadcrumb-item text-capitalize active">{{ __('Dashboard') }}</li>
+    <li class="breadcrumb-item text-capitalize">{{ __('Sports') }}</li>
+    <li class="breadcrumb-item text-capitalize active">{{ __('All Sports') }}</li>
 @endsection
 
+@section('breadcrumb_buttons')
+    <a href="{{ route('administration.sport.create') }}" class="btn btn-outline-dark btn-outline-custom fw-bolder">
+        <i class="feather icon-plus"></i>
+        <b>Create New Sport</b>
+    </a>
+@endsection
 
 
 @section('content')
 
-<!-- Start row -->
 <!-- Start row -->
 <div class="row">
     <!-- Start col -->
     <div class="col-lg-12">
         <div class="card m-b-30">
             <div class="card-header">
-                <div class="row align-items-center">
-                    <div class="col-6">
-                        <h5 class="card-title mb-0">All Season</h5>
-                    </div>
-                </div>
+                <h5 class="card-title">{{ __('All Sports') }}</h5>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table table-borderless">
+                    <table id="default-datatable" class="display table table-bordered">
                         <thead>
                             <tr>
                                 <th>S/N</th>
                                 <th>Sport</th>
+                                <th>Description</th>
                                 <th>Status</th>
-                                <th>Season</th>
-                                <th>Actions</th>
+                                <th class="text-right">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Basketball</td>
-                                <td>
-                                    <span class="badge badge-secondary badge-success"><span class="fa fa-xs fa-check"></span> Active</span>
-                                    <span class="badge badge-secondary badge-info"><span class="fa fa-xs fa-check"></span> Default</span>
-                                </td>
-                                <td><a href="#">2023-2024 Winter</a></td>
-                                <td>
-                                    <div class="button-list">
-                                        <a href="#" class="btn btn-primary-rgba" title="Details"><i class="feather icon-file"></i></a>
-                                        <a href="add_season.html" class="btn btn-success-rgba"><i class="feather icon-edit-2"></i></a>
-                                        <a href="#" class="btn btn-danger-rgba"><i class="feather icon-trash"></i></a>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Football</td>
-                                <td>
-                                    <span class="badge badge-secondary badge-success"><span class="fa fa-xs fa-check"></span> Active</span>
-                                </td>
-                                <td><a href="#">2023 2023</a></td>
-                                <td>
-                                    <div class="button-list">
-                                        <a href="#" class="btn btn-primary-rgba" title="Details"><i class="feather icon-file"></i></a>
-                                        <a href="add_season.html" class="btn btn-success-rgba"><i class="feather icon-edit-2"></i></a>
-                                        <a href="#" class="btn btn-danger-rgba"><i class="feather icon-trash"></i></a>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Basketball</td>
-                                <td>
-                                    <span class="badge badge-secondary badge-success"><span class="fa fa-xs fa-check"></span> Active</span>
-                                </td>
-                                <td><a href="#">2023</a></td>
-                                <td>
-                                    <div class="button-list">
-                                        <a href="#" class="btn btn-primary-rgba" title="Details"><i class="feather icon-file"></i></a>
-                                        <a href="add_season.html" class="btn btn-success-rgba"><i class="feather icon-edit-2"></i></a>
-                                        <a href="#" class="btn btn-danger-rgba"><i class="feather icon-trash"></i></a>
-                                    </div>
-                                </td>
-                            </tr>
+                            @foreach ($sports as $sl => $sport)
+                                <tr>
+                                    <th class="fw-bold"><b>#{{ $sl+1 }}</b></th>
+                                    <td>{{ $sport->name }}</td>
+                                    <td>{{ $sport->description }}</td>
+                                    <td>{!! status($sport->status) !!}</td>
+                                    <td class="text-right">
+                                        <div class="action-btn-group mr-3">
+                                            <a href="#" class="btn btn-outline-danger btn-outline-custom btn-sm" data-toggle="tooltip" data-placement="top" title="{{ __('Delete?') }}" onclick="return confirm('Are You Sure Want To Delete?');">
+                                                <i class="feather icon-trash-2"></i>
+                                            </a>
+                                            <a href="#" class="btn btn-outline-info btn-outline-custom btn-sm" data-toggle="tooltip" data-placement="top" title="{{ __('View?') }}">
+                                                <i class="feather icon-info"></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -113,18 +94,34 @@
     <!-- End col -->
 </div>
 <!-- End row -->
-<!-- End row -->
+
 
 @endsection
 
 
 @section('script_links')
-    {{--  External Javascript Links --}}   
+    {{--  External Javascript Links --}}
+    <!-- Datatable js -->
+    <script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/jszip.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/js/custom/custom-table-datatable.js') }}"></script>
 @endsection
 
 @section('custom_script')
     {{--  External Custom Javascript  --}}
     <script>
         // Custom Script Here
+        /* -- Bootstrap Tooltip -- */
+        $('[data-toggle="tooltip"]').tooltip();
     </script>
 @endsection
