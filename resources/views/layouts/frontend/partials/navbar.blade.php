6a0ed4da58762b16c3c2670452aee7d770947274
NIKHIL KURMI <nikhil.lu210@gmail.com>
2023-08-06 22:52:31 +0600

    Update navbar.blade.php

--- resources/views/layouts/frontend/partials/navbar.blade.php
+++ resources/views/layouts/frontend/partials/navbar.blade.php
@@ -1,34 +1,34 @@
 <header class="float-start w-100">
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-            <a class="navbar-brand" href="index.html">
+            <a class="navbar-brand" href="{{ route('frontend.homepage.index') }}">
                 <img src="{{ asset('frontend/images/logo.png') }}" alt="logo" />
             </a>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('frontend.homepage.index') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.html">About Club</a>
+                        <a class="nav-link" href="#">About Club</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="matches.html">Matches</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="shop.html">Shop</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="blog.html">Blog</a>
+                        <a class="nav-link" href="#">Matches</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="players.html">Players</a>
+                        <a class="nav-link" href="#">Shop</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact</a>
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Players</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -84,10 +84,10 @@
                                 </div>
                             </li>
                             <li>
-                                <a href="cart.html" class="btn cart-drop-bn"> View Cart </a>
+                                <a href="#" class="btn cart-drop-bn"> View Cart </a>
                             </li>
                             <li>
-                                <a href="checkout.html" class="btn check-drop-bn"> Check out </a>
+                                <a href="#" class="btn check-drop-bn"> Check out </a>
                             </li>
                         </ul>
                     </li>
@@ -95,17 +95,6 @@
                         <button type="button" data-bs-toggle="modal" data-bs-target="#loginModal" class="btn login-btn-links">
                             <span class="m-0 oipn">
                                 <img src="{{ asset('frontend/images/747376.png') }}" alt="pnm" />
-                            </span>
-                        </button>
-                    </li>
-                    <li class="d-block d-lg-block">
-                        <button type="button" class="btn bar-btn-links" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRightmobile">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
-                                    <path
-                                        d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z"
-                                    />
-                                </svg>
                             </span>
                         </button>
                     </li>
