8685cb05c0044d030b55ad642227e1ee5358f82e
Md Omor Faruk <mdomorfaruk.lu.cse@gmail.com>
2023-08-22 13:01:48 +0600

    Working on Sponosr

--- resources/views/administration/sponsor/create.blade.php
+++ resources/views/administration/sponsor/create.blade.php
@@ -5,7 +5,7 @@
 
 @endsection
 
-@section('page_title', __('Create New Sport'))
+@section('page_title', __('Create New Sponsor'))
 
 @section('css_links')
     {{--  External CSS  --}}
@@ -20,17 +20,77 @@
     {{--  External CSS  --}}
     <style>
     /* Custom CSS Here */
+    /* Image Upload */
+    .avatar-upload {
+        position: relative;
+        max-width: 205px;
+        margin: 50px auto;
+    }
+    .avatar-upload .avatar-edit {
+        position: absolute;
+        right: 12px;
+        z-index: 1;
+        top: 10px;
+    }
+    .avatar-upload .avatar-edit input {
+        display: none;
+    }
+    .avatar-upload .avatar-edit input + label {
+        display: inline-block;
+        width: 34px;
+        height: 34px;
+        margin-bottom: 0;
+        border-radius: 100%;
+        background: #ffffff;
+        border: 1px solid;
+        border-color: #a1a1a1;
+        box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.12);
+        cursor: pointer;
+        font-weight: normal;
+        transition: all 0.2s ease-in-out;
+    }
+    .avatar-upload .avatar-edit input + label:hover {
+        background: #d8d8d8;
+        border-color: #a1a1a1;
+    }
+    .avatar-upload .avatar-edit input + label:after {
+        content: "\f040";
+        font-family: "FontAwesome";
+        color: #757575;
+        position: absolute;
+        top: 5px;
+        left: 0;
+        right: 0;
+        text-align: center;
+        margin: auto;
+    }
+    .avatar-upload .avatar-preview {
+        width: 192px;
+        height: 192px;
+        position: relative;
+        border-radius: 100%;
+        border: 6px solid #f8f8f8;
+        box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
+    }
+    .avatar-upload .avatar-preview > div {
+        width: 100%;
+        height: 100%;
+        border-radius: 100%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
     </style>
 @endsection
 
 
 @section('page_name')
-    <b class="text-uppercase">{{ __('Create New Sport') }}</b>
+    <b class="text-uppercase">{{ __('Create New Sponsor') }}</b>
 @endsection
 
 
 @section('breadcrumb')
-    <li class="breadcrumb-item text-capitalize active">{{ __('Create New Sport') }}</li>
+    <li class="breadcrumb-item text-capitalize active">{{ __('Create New Sponsor') }}</li>
 @endsection
 
 
@@ -41,7 +101,7 @@
 <!-- Start Row -->
 <div class="row justify-content-center">
     <div class="col-md-12">
-        <form action="{{ route('administration.sport.store') }}" method="post" enctype="multipart/form-data" autocomplete="off">
+        <form action="{{ route('administration.sponsor.store') }}" method="post" enctype="multipart/form-data" autocomplete="off">
             @csrf
             <div class="card m-b-30">
                 <div class="card-body">
@@ -49,10 +109,21 @@
                         <div class="col-md-12">
                             <div class="card border m-b-30">
                                 <div class="card-header border-bottom">
-                                    <h5 class="card-title mb-0">Create New Sport</h5>
+                                    <h5 class="card-title mb-0">Create New Sponsor</h5>
                                 </div>
                                 <div class="card-body">
                                     <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="avatar-upload">
+                                                <div class="avatar-edit">
+                                                    <input type="file" id="sponsorAvatar" name="avatar" accept=".png, .jpg, .jpeg" />
+                                                    <label for="sponsorAvatar"></label>
+                                                </div>
+                                                <div class="avatar-preview">
+                                                    <div id="imagePreview" style="background-image: url(https://fakeimg.pl/500x500);"></div>
+                                                </div>
+                                            </div>
+                                        </div>
                                         <div class="col-md-6 form-group">
                                             <label for="name">Name <span class="required">*</span></label>
                                             <input type="text" name="name" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror" placeholder="Tennies" required/>
@@ -88,7 +159,7 @@
                 <div class="card-footer">
                     <button type="submit" class="btn btn-outline-primary btn-outline-custom float-right">
                         <i class="feather icon-plus mr-1"></i>
-                        <span class="text-bold">Create New Sport</span>
+                        <span class="text-bold">Create New sponsor</span>
                     </button>
                 </div>
             </div>
@@ -111,5 +182,20 @@
     {{--  External Custom Javascript  --}}
     <script>
         // Custom Script Here
+        // File Uploder
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+                    $('#imagePreview').hide();
+                    $('#imagePreview').fadeIn(650);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+        $("sponsorAvatar").change(function() {
+            readURL(this);
+        });
     </script>
 @endsection
