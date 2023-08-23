8e7ed7f249a21836329c69db71071732e53e2545
mdomorfarukcse <mdomorfaruk.lu.cse@gmail.com>
2023-08-23 18:12:49 +0600

    Tracking Update

--- resources/views/frontend/shop/order/show.blade.php
+++ resources/views/frontend/shop/order/show.blade.php
@@ -67,96 +67,98 @@
                             </div>
                         </form>
                     </div>
-                    <div class="col-lg-8 mb-5">
-                        <div class="total-count-div">
-                            <h4>Order Details Summary</h4>
-                            <hr class="my-2" />
-                            <div class="col-12">
-                                <div class="table-responsive">
-                                    <table class="table table-responsive table-bordered">
-                                        <tbody>
-                                            <tr>
-                                                <th>Order ID</th>
-                                                <td>
-                                                    <b class="text-dark">
-                                                        {{ $order->order_id }}
-                                                    </b>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>Status</th>
-                                                <td>{{ $order->status }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th>Order Placed At</th>
-                                                <td>{{ date_time_ago($order->created_at) }}</td>
-                                            </tr>
-                                            <tr>
-                                                <th>Cutomer Info</th>
-                                                <td>
-                                                    <p>
-                                                        <b>Name:</b>
-                                                        <span>{{ $order->name }}</span>
-                                                    </p>
-                                                    <p>
-                                                        <b>Email:</b>
-                                                        <span>{{ $order->email }}</span>
-                                                    </p>
-                                                    <p>
-                                                        <b>Phone:</b>
-                                                        <span>{{ $order->contact_number }}</span>
-                                                    </p>
-                                                    <p>
-                                                        <b>Address:</b>
-                                                        <span>{{ $order->address }}</span>
-                                                    </p>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>Products</th>
-                                                <td>
-                                                    <ol style="margin-left: 15px;">
-                                                        @foreach ($order->products as $product)
-                                                            <li>
-                                                                <a href="{{ route('frontend.shop.show', ['product' => $product]) }}" class="text-info">
-                                                                    <b>{{ $product->name }}</b>
-                                                                </a>
-                                                                <p>
-                                                                    <span>
-                                                                        <b>Price: </b> {{ $product->pivot->price }}
-                                                                    </span>
-                                                                    |
-                                                                    <span>
-                                                                        <b>Quantity: </b> {{ $product->pivot->quantity }}
-                                                                    </span>
-                                                                    |
-                                                                    <span>
-                                                                        <b>Color: </b> {{ $product->pivot->color }}
-                                                                    </span>
-                                                                    |
-                                                                    <span>
-                                                                        <b>Size: </b> {{ $product->pivot->size }}
-                                                                    </span>
-                                                                    |
-                                                                    <span>
-                                                                        <b>Total: </b> {{ $product->pivot->total }}
-                                                                    </span>
-                                                                </p>
-                                                            </li>
-                                                        @endforeach
-                                                    </ol>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>Sub Total</th>
-                                                <td>{{ $order->total_price }}</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                    @if (!is_null($order))
+                        <div class="col-lg-8 mb-5">
+                            <div class="total-count-div">
+                                <h4>Order Details Summary</h4>
+                                <hr class="my-2" />
+                                <div class="col-12">
+                                    <div class="table-responsive">
+                                        <table class="table table-responsive table-bordered">
+                                            <tbody>
+                                                <tr>
+                                                    <th>Order ID</th>
+                                                    <td>
+                                                        <b class="text-dark">
+                                                            {{ $order->order_id }}
+                                                        </b>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Status</th>
+                                                    <td>{{ $order->status }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Order Placed At</th>
+                                                    <td>{{ date_time_ago($order->created_at) }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Cutomer Info</th>
+                                                    <td>
+                                                        <p>
+                                                            <b>Name:</b>
+                                                            <span>{{ $order->name }}</span>
+                                                        </p>
+                                                        <p>
+                                                            <b>Email:</b>
+                                                            <span>{{ $order->email }}</span>
+                                                        </p>
+                                                        <p>
+                                                            <b>Phone:</b>
+                                                            <span>{{ $order->contact_number }}</span>
+                                                        </p>
+                                                        <p>
+                                                            <b>Address:</b>
+                                                            <span>{{ $order->address }}</span>
+                                                        </p>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Products</th>
+                                                    <td>
+                                                        <ol style="margin-left: 15px;">
+                                                            @foreach ($order->products as $product)
+                                                                <li>
+                                                                    <a href="{{ route('frontend.shop.show', ['product' => $product]) }}" class="text-info">
+                                                                        <b>{{ $product->name }}</b>
+                                                                    </a>
+                                                                    <p>
+                                                                        <span>
+                                                                            <b>Price: </b> {{ $product->pivot->price }}
+                                                                        </span>
+                                                                        |
+                                                                        <span>
+                                                                            <b>Quantity: </b> {{ $product->pivot->quantity }}
+                                                                        </span>
+                                                                        |
+                                                                        <span>
+                                                                            <b>Color: </b> {{ $product->pivot->color }}
+                                                                        </span>
+                                                                        |
+                                                                        <span>
+                                                                            <b>Size: </b> {{ $product->pivot->size }}
+                                                                        </span>
+                                                                        |
+                                                                        <span>
+                                                                            <b>Total: </b> {{ $product->pivot->total }}
+                                                                        </span>
+                                                                    </p>
+                                                                </li>
+                                                            @endforeach
+                                                        </ol>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Sub Total</th>
+                                                    <td>{{ $order->total_price }}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 </div>
             </div>
         </div>
