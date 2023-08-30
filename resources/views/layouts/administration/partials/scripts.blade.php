814171feecf7996df580a30e678ef0c5bf5b5fcc
nigelsi2023 <nigel.pi@staff-india.com>
2023-08-30 17:26:38 +0600

    File Changes and Console errors solved

--- resources/views/layouts/administration/partials/scripts.blade.php
+++ resources/views/layouts/administration/partials/scripts.blade.php
@@ -1,18 +1,13 @@
-<!-- Start js -->
+<!-- Start js -->        
 <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 <script src="{{ asset('assets/js/popper.min.js') }}"></script>
 <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/modernizr.min.js') }}"></script>
+<script src="{{ asset('assets/js/detect.js') }}"></script>
 <script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
 <script src="{{ asset('assets/js/vertical-menu.js') }}"></script>
 <!-- Switchery js -->
 {{-- <script src="{{ asset('assets/plugins/switchery/switchery.min.js') }}"></script> --}}
-<!-- Select2 js -->
-<script src="{{ asset('assets/plugins/select2/select2.min.js') }}"></script>
-
-<script>
-    $('#selectRoleNavbar').select2();
-</script>
-
 
 @yield('script_links')
 
