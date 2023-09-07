96e3ad931002cf2e04cea507353193bd891c81f0
nigelsi2023 <nigel.pi@staff-india.com>
2023-09-07 17:17:14 +0600

    Coach And Player Show Update

--- app/Http/Controllers/Administration/Coach/CoachController.php
+++ app/Http/Controllers/Administration/Coach/CoachController.php
@@ -35,7 +35,7 @@ class CoachController extends Controller
      */
     public function create()
     {
-        $coach_id = $this->generateUniqueID();
+        $coach_id = unique_id(11, 11);
         return view('administration.coach.create', compact('coach_id'));
     }
 
@@ -175,36 +175,5 @@ class CoachController extends Controller
             alert('Coach Deletation Failed!', 'There is some error! Please fix and try again.', 'error');
             return redirect()->back()->withInput();
         }
-    }
-
-
-    // Generate a unique ID with a minimum and maximum length of 10 characters
-    private function generateUniqueID() {
-        $length = 10;
-        $timestampLength = 13; // Length of the timestamp in milliseconds
-        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    
-        // Get the current timestamp in milliseconds
-        $timestamp = round(microtime(true) * 1000);
-    
-        // Convert the timestamp to a string and remove the decimal point
-        $timestampString = str_replace('.', '', (string)$timestamp);
-    
-        // Calculate the number of characters needed to fill the remaining length
-        $charactersNeeded = $length - $timestampLength;
-    
-        // Ensure we have enough characters to fill the length
-        while (strlen($timestampString) < $charactersNeeded) {
-            $randomCharacter = $characters[random_int(0, strlen($characters) - 1)];
-            $timestampString .= $randomCharacter;
-        }
-    
-        // Convert the timestamp to all capital letters
-        $timestampString = strtoupper($timestampString);
-    
-        // Combine the timestamp with the random characters and take the first $length characters
-        $uniqueID = substr($timestampString, 0, $length);
-    
-        return $uniqueID;
     }
 }
--- resources/views/administration/coach/show.blade.php
+++ resources/views/administration/coach/show.blade.php
@@ -9,6 +9,11 @@
 
 @section('css_links')
     {{--  External CSS  --}}
+    <!-- DataTables css -->
+    <link href="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Responsive Datatable css -->
+    <link href="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 
 @section('custom_css')
@@ -130,6 +135,79 @@
             </div>
         </div>
     </div>
+
+    <div class="col-lg-12">
+        <div class="card m-b-30">
+            <div class="card-header">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <h5 class="card-title mb-0">All Teams of <b class="text-info">{{ $coach->user->name }}</b></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="default-datatable" class="display table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Event</th>
+                                <th>Players</th>
+                                <th>Status</th>
+                                @if (auth()->user()->can('team.show') || auth()->user()->can('team.destroy')) 
+                                    <th class="text-right">Actions</th>
+                                @endif
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($coach->teams as $key => $team)
+                                <tr>
+                                    <td class="fw-bold text-dark"><b>#{{ serial($coach->teams, $key) }}</b></th>
+                                    <td>
+                                        <span class="text-dark text-capitalize">{{ $team->name }}</span>
+                                        <br>
+                                        <small class="text-muted">{{ $team->division->name }}</small>
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('administration.event.show', ['event' => $team->event]) }}" target="_blank" class="text-bold text-dark" data-toggle="tooltip" data-placement="top" title="{{ __('Click to see '.$team->event->name.'\'s Details') }}">
+                                            {{ $team->event->name }}
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <span class="text-success text-bold">
+                                            {{ count($team->players) }}
+                                        </span>
+                                        / 
+                                        <span class="text-dark text-bold">
+                                            {{ $team->maximum_players }}
+                                        </span>
+                                    </td>
+                                    <td>{!! status($team->status) !!}</td>
+                                    @if (auth()->user()->can('team.show') || auth()->user()->can('team.destroy')) 
+                                        <td class="text-right">
+                                            <div class="action-btn-group mr-3">
+                                                @if (auth()->user()->can('team.destroy')) 
+                                                    <a href="javascript:void(0);" class="btn btn-outline-danger btn-outline-custom btn-sm" data-toggle="tooltip" data-placement="top" title="{{ __('Delete?') }}" onclick="return confirm('Are You Sure Want To Delete?');">
+                                                        <i class="feather icon-trash-2"></i>
+                                                    </a>
+                                                @endif
+                                                @if (auth()->user()->can('team.show')) 
+                                                    <a href="{{ route('administration.team.show', ['team' => $team]) }}" class="btn btn-outline-info btn-outline-custom btn-sm" data-toggle="tooltip" data-placement="top" title="{{ __('View?') }}">
+                                                        <i class="feather icon-info"></i>
+                                                    </a>
+                                                @endif
+                                            </div>
+                                        </td>
+                                    @endif
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <!-- End Row -->
@@ -139,8 +217,27 @@
 
 @section('script_links')
     {{--  External Javascript Links --}}
+    <!-- Datatable js -->
+    <script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/jszip.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/js/custom/custom-table-datatable.js') }}"></script>
 @endsection
 
 @section('custom_script')
     {{--  External Custom Javascript  --}}
+    <script>
+        // Custom Script Here
+        /* -- Bootstrap Tooltip -- */
+        $('[data-toggle="tooltip"]').tooltip();
+    </script>
 @endsection
