bd32039afbf12371585866128524063bf117bab8
nigelsi2023 <nigel.pi@staff-india.com>
2023-09-07 17:07:06 +0600

    Sport Show Modified

--- resources/views/administration/sport/show.blade.php
+++ resources/views/administration/sport/show.blade.php
@@ -9,6 +9,11 @@
 
 @section('css_links')
     {{--  External CSS  --}}
+    <!-- DataTables css -->
+    <link href="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Responsive Datatable css -->
+    <link href="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 
 @section('custom_css')
@@ -85,6 +90,69 @@
             </div>
         </div>
     </div>
+
+    <div class="col-lg-12">
+        <div class="card m-b-30">
+            <div class="card-header">
+                <h5 class="card-title">{{ __('All Events Under ') }} <span class="text-bold text-info">{{ $sport->name }}</span></h5>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="default-datatable" class="display table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Sl.</th>
+                                <th>Logo</th>
+                                <th>Name</th>
+                                <th>Reg. Fee</th>
+                                <th>Divisions</th>
+                                <th>Venues</th>
+                                <th>Status</th>
+                                @if (auth()->user()->can('event.show') || auth()->user()->can('event.destroy')) 
+                                    <th class="text-right">Actions</th>
+                                @endif
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($sport->events as $key => $event)
+                                <tr>
+                                    <td class="fw-bold text-dark"><b>#{{ serial($sport->events, $key) }}</b></th>
+                                    <td>
+                                        <img src="{{ show_avatar($event->logo) }}" class="img-fluid img-thumbnail rounded-circle table-avatar" height="50" width="50" alt="event">
+                                    </td>
+                                    <td>
+                                        {{ $event->name }}
+                                        <br>
+                                        <small class="text-muted">{{ $event->season->name }} ({{ $event->sport->name }})</small>
+                                    </td>
+                                    <td class="text-bold text-primary">${{ $event->registration_fee }}</td>
+                                    <td>{{ count($event->divisions) }}</td>
+                                    <td>{{ count($event->venues) }}</td>
+                                    <td>{!! status($event->status) !!}</td>
+                                    @if (auth()->user()->can('event.show') || auth()->user()->can('event.destroy')) 
+                                        <td class="text-right">
+                                            <div class="action-btn-group mr-3">
+                                                @if (auth()->user()->can('event.destroy')) 
+                                                    <a href="{{ route('administration.event.destroy', ['event' => $event]) }}" class="btn btn-outline-danger btn-outline-custom btn-sm" data-toggle="tooltip" data-placement="top" title="{{ __('Delete?') }}" onclick="return confirm('Are You Sure Want To Delete?');">
+                                                        <i class="feather icon-trash-2"></i>
+                                                    </a>
+                                                @endif
+                                                @if (auth()->user()->can('event.show')) 
+                                                    <a href="{{ route('administration.event.show', ['event' => $event]) }}" class="btn btn-outline-info btn-outline-custom btn-sm" data-toggle="tooltip" data-placement="top" title="{{ __('View?') }}">
+                                                        <i class="feather icon-info"></i>
+                                                    </a>
+                                                @endif
+                                            </div>
+                                        </td>
+                                    @endif
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <!-- End Row -->
@@ -94,8 +162,27 @@
 
 @section('script_links')
     {{--  External Javascript Links --}}
+    <!-- Datatable js -->
+    <script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/jszip.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/js/custom/custom-table-datatable.js') }}"></script>
 @endsection
 
 @section('custom_script')
     {{--  External Custom Javascript  --}}
+    <script>
+        // Custom Script Here
+        /* -- Bootstrap Tooltip -- */
+        $('[data-toggle="tooltip"]').tooltip();
+    </script>
 @endsection
