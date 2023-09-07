75f4f1624c70b52b547b466915f7bffad65da793
nigelsi2023 <nigel.pi@staff-india.com>
2023-09-07 16:02:40 +0600

    Season Permissions Done

--- resources/views/administration/season/show.blade.php
+++ resources/views/administration/season/show.blade.php
@@ -38,12 +38,14 @@
 @endsection
 
 
-@section('breadcrumb_buttons')
-    <a href="{{ route('administration.season.edit', ['season' => $season]) }}" class="btn btn-outline-dark btn-outline-custom fw-bolder">
-        <i class="feather icon-pen"></i>
-        <b>Edit Season Info</b>
-    </a>
-@endsection
+@if (auth()->user()->can('season.update')) 
+    @section('breadcrumb_buttons')
+        <a href="{{ route('administration.season.edit', ['season' => $season]) }}" class="btn btn-outline-dark btn-outline-custom fw-bolder">
+            <i class="feather icon-pen"></i>
+            <b>Edit Season Info</b>
+        </a>
+    @endsection
+@endif
 
 
 
@@ -52,44 +54,41 @@
 <!-- Start Row -->
 <div class="row justify-content-center">
     <div class="col-md-12">
-        <form action="{{ route('administration.season.update', ['season' => $season]) }}" method="post" enctype="multipart/form-data" autocomplete="off">
-            @csrf
-            <div class="card m-b-30">
-                <div class="card-body">
-                    <div class="row justify-content-center">
-                        <div class="col-md-12">
-                            <div class="card border">
-                                <div class="card-header bg-primary-rgba border-bottom">
-                                    <h5 class="card-title text-primary mb-0">Season's Information</h5>
-                                </div>
-                                <div class="card-body py-2">
-                                    <div class="row">
-                                        <div class="table-responsive">
-                                            <table class="table table-bordered mb-0">
-                                                <tbody>
-                                                    <tr>
-                                                        <th>Season Name</th>
-                                                        <td>{{ $season->name }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>Year</th>
-                                                        <td>{{ $season->year }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>Started At</th>
-                                                        <td>{{ $season->start }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>Ends At</th>
-                                                        <td>{{ $season->end }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>Status</th>
-                                                        <td>{!! status($season->status) !!}</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
+        <div class="card m-b-30">
+            <div class="card-body">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card border">
+                            <div class="card-header bg-primary-rgba border-bottom">
+                                <h5 class="card-title text-primary mb-0">Season's Information</h5>
+                            </div>
+                            <div class="card-body py-2">
+                                <div class="row">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered mb-0">
+                                            <tbody>
+                                                <tr>
+                                                    <th>Season Name</th>
+                                                    <td>{{ $season->name }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Year</th>
+                                                    <td>{{ $season->year }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Started At</th>
+                                                    <td>{{ $season->start }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Ends At</th>
+                                                    <td>{{ $season->end }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Status</th>
+                                                    <td>{!! status($season->status) !!}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
                                     </div>
                                 </div>
                             </div>
@@ -97,7 +96,7 @@
                     </div>
                 </div>
             </div>
-        </form>
+        </div>
     </div>
     
     <div class="col-md-12">
