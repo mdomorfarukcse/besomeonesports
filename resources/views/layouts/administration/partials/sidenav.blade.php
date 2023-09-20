60110367b8a1e1146ca438b8d67e88ba30b272a2
nigelsi2023 <nigel.pi@staff-india.com>
2023-09-20 17:50:01 +0600

    Seeder Update

--- resources/views/layouts/administration/partials/sidenav.blade.php
+++ resources/views/layouts/administration/partials/sidenav.blade.php
@@ -380,54 +380,6 @@
                         <span>Website</span> 
                     </a>
                 </li>
-
-
-                {{-- @role('developer')
-                    <li>
-                        <a href="javaScript:void(0);">
-                            <i class="sl-icon-settings"></i>
-                            <span>{{ __('Settings') }}</span>
-                            <i class="feather icon-chevron-right pull-right"></i>
-                        </a>
-                        <ul class="vertical-submenu">
-                            <li>
-                                <a href="javaScript:void(0);">{{ __('Permission') }}
-                                    <i class="feather icon-chevron-right pull-right"></i>
-                                </a>
-                                <ul class="vertical-submenu">
-                                    <li>
-                                        <a href="{{ route('administration.settings.permission.index') }}">
-                                            {{ __('All Permissions') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('administration.settings.permission.group.index') }}">
-                                            {{ __('Permission Groups') }}
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <a href="javaScript:void(0);">{{ __('Role') }}
-                                    <i class="feather icon-chevron-right pull-right"></i>
-                                </a>
-                                <ul class="vertical-submenu">
-                                    <li>
-                                        <a href="{{ route('administration.settings.role.index') }}">
-                                            {{ __('All Roles') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('administration.settings.role.create') }}">
-                                            {{ __('Create Role') }}
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                @endrole --}}
             </ul>
         </div>
         <!-- End Navigationbar -->
