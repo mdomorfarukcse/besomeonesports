f5919e7f962d8a8c421f8d00d71e564e8578ef92
Nigel Staff-India <124186672+nigelsi2023@users.noreply.github.com>
2023-09-27 22:13:13 +0600

    Player Registration Updated (#54)

---
+++ app/Http/Requests/Administration/Player/NewPlayerUpdateRequest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Requests\Administration\Player;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NewPlayerUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'player_id' => ['required', 'string', 'unique:players,player_id'],
+            "first_name" => ['required', 'max:50'],
+            "middle_name" => ['nullable', 'string', 'max:50'],
+            "last_name" => ['required', 'max:50'],
+            "birthdate" => ['required', 'date', 'date_format:Y-m-d'],
+            "contact_number" => ['required', 'string', 'max:20'],
+            "city" => ['required', 'string', 'max:50'],
+            "state" => ['required', 'string', 'max:50'],
+            "postal_code" => ['required', 'string', 'max:10'],
+            "street_address" => ['required', 'string', 'max:100'],
+            "extended_address" => ['string', 'max:100'],
+            "position" => ['nullable', 'string'],
+            "height" => ['nullable', 'numeric', 'regex:/^\d+(\.\d{1,2})?$/'],
+            "weight" => ['nullable', 'numeric', 'regex:/^\d+(\.\d{1,2})?$/'],
+            "note" => ['nullable', 'string'],
+
+            // Parents Info
+            "father_name" => ['required', 'string'],
+            "father_email" => ['nullable', 'email'],
+            "father_contact" => ['nullable', 'string', 'max:20'],
+            "mother_name" => ['required', 'string'],
+            "mother_email" => ['nullable', 'email'],
+            "mother_contact" => ['nullable', 'string', 'max:20'],
+
+            // Guardian Info
+            "guardian_relation" => ['required', 'string'],
+            "guardian_name" => ['required', 'string'],
+            "guardian_email" => ['nullable', 'email'],
+            "guardian_contact" => ['required', 'string', 'max:20'],
+        ];
+    }
+    
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'player_id.unique' => 'The Player ID Already exists. It should be Unique.',
+            'status.in' => 'The Status should be Active or Inactive only.',
+        ];
+    }
+}
