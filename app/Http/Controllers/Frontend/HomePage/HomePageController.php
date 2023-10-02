07a48ff7d4f1c097eb66c051071cf58fa498af7d
mdomorfarukcse <mdomorfaruk.lu.cse@gmail.com>
2023-10-02 21:42:00 +0600

    ads show on home

--- app/Http/Controllers/Frontend/HomePage/HomePageController.php
+++ app/Http/Controllers/Frontend/HomePage/HomePageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend\HomePage;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ads\Ads;
 use App\Models\Coach\Coach;
 use App\Models\League\League;
 use App\Models\Gallery\Gallery;
@@ -17,6 +18,7 @@ class HomePageController extends Controller
 {
     function index() {
 
+        $today = now()->toDateString(); 
         $upcomingLeagues = League::whereStatus('Active')
                                 ->orderBy('start', 'asc')
                                 ->get();
@@ -24,8 +26,18 @@ class HomePageController extends Controller
         
         $galleries = Gallery::limit(8)->get();
         $videos = Video::whereStatus('Active')->limit(8)->get();
-        
-
+        $top_ad = Ads::whereDate('startdate', '<=', $today)
+                    ->whereDate('enddate', '>=', $today)
+                    ->wherePosition('hometop')
+                    ->whereStatus('Active')
+                    ->inRandomOrder()
+                    ->first();
+        $bottom_ad = Ads::whereDate('startdate', '<=', $today)
+                    ->whereDate('enddate', '>=', $today)
+                    ->wherePosition('homebottom')
+                    ->whereStatus('Active')
+                    ->inRandomOrder()
+                    ->first();
         $teams = Team::whereStatus('Active')->count();
         $leagues = League::whereStatus('Active')->count();
         $coaches = Coach::whereStatus('Active')->count();
@@ -37,6 +49,6 @@ class HomePageController extends Controller
             'coaches' => $coaches,
             'players' => $players
         ];                  
-        return view('frontend.homepage.index', compact(['upcomingLeagues','products','total','galleries','videos']));
+        return view('frontend.homepage.index', compact(['upcomingLeagues','products','total','galleries','videos','top_ad','bottom_ad']));
     }
 }
