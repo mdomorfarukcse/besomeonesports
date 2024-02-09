3bd393f056e9c2307ee9f762f8f81197868c7f7e
Md Omor Faruk <mdomorfaruk.lu.cse@gmail.com>
2024-02-09 23:37:52 +0600

    Menu Update

--- resources/views/layouts/frontend/partials/navbar.blade.php
+++ resources/views/layouts/frontend/partials/navbar.blade.php
@@ -91,21 +91,60 @@
                             </span>
                         </a>
                     </li>
-                    <li>
-                        <button type="button" class="btn bar-btn-links" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRightmobile">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
-                                    <path
-                                        d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z"
-                                    />
-                                </svg>
-                            </span>
-                        </button>
-                    </li>
-                    
                 </ul>
             </div>
         </div>
     </nav>
+    <style>
+    .mobilenav{
+        display: none;
+    }
+    .mobilenav ul {
+        background: #01aeed;
+        color: #fff;
+        padding: 5px 10px;
+    }
+    .mobilenav li {
+        display: inline-block;
+    }
+    .mobilenav ul li:first-child {
+        margin-left: 5%;
+    }
+    .mobilenav ul li:nth-child(2) {
+        margin-left: 65%;
+    }
+    .mobilenav ul li:nth-child(2) i {
+        color: #fff;
+        font-size: 22px;
+    }
+    /* Extra small devices (phones, 600px and down) */
+    @media only screen and (max-width: 600px) {
+        .mobilenav{
+            display: block;
+        }
+    }
+    /* Small devices (portrait tablets and large phones, 600px and up) */
+    @media only screen and (max-width: 985px) {
+        .mobilenav{
+            display: block;
+        }
+        .mobilenav ul li:nth-child(2) {
+            margin-left: 65%;
+        }
+    }
+    </style>
+    <div class="mobilenav">
+        <ul>
+            <li>
+                <h5>Menu</h5>
+            </li>
+            <li>
+                <button type="button" class="btn bar-btn-links" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRightmobile">
+                    <i class="fas fa-bars" aria-hidden="true"></i>
+                </button>
+            </li>
+        </ul>
+        
+    </div>
 </header>
 
