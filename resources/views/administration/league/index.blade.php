31caf9516c9c45288b04b8379ad08fef30ac7769
nikhil-lu210 <nikhil.lu210@gmail.com>
2024-03-13 02:33:38 +0600

    League Filter Update

--- resources/views/administration/league/index.blade.php
+++ resources/views/administration/league/index.blade.php
@@ -82,24 +82,39 @@
                                 <b class="text-danger"><i class="feather icon-info mr-1"></i>{{ $message }}</b>
                             @enderror
                         </div>
-                        <div class="form-group col-md-4">
+                        <div class="form-group col-md-3">
                             <label for="name">League Name</label>
                             <input type="text" name="name" value="{{ $request->name ? $request->name : old('name') }}" class="form-control @error('name') is-invalid @enderror" placeholder="Fifa World Club 2023"/>
                             @error('name')
                                 <b class="text-danger"><i class="feather icon-info mr-1"></i>{{ $message }}</b>
                             @enderror
                         </div>
-                        <div class="col-md-2">
-                            <button type="submit" class="btn btn-dark btn-custom btn-block m-t-30">
-                                <i class="feather icon-filter mr-1"></i>
-                                <span class="text-bold">Filter</span>
-                            </button>
-                            @if ($request->filter == true) 
-                                <a href="{{ route('administration.league.index') }}" class="btn btn-link text-danger text-bold float-end float-right">
-                                    <i class="icon feather icon-x m-r-1"></i>
-                                    Clear Filter
-                                </a>
-                            @endif
+                        <div class="col-md-3">
+                            <div class="row">
+                                <div class="form-group col-md-7">
+                                    <label for="status">Status</label>
+                                    <select class="select2-single form-control @error('status') is-invalid @enderror" name="status">
+                                        <option value="">Select Status</option>
+                                        <option value="Active" {{ $request->status === 'Active' ? 'selected' : '' }}>Active</option>
+                                        <option value="Inactive" {{ $request->status === 'Inactive' ? 'selected' : '' }}>Inactive</option>
+                                    </select>
+                                    @error('status')
+                                        <b class="text-danger"><i class="feather icon-info mr-1"></i>{{ $message }}</b>
+                                    @enderror
+                                </div>
+                                <div class="col-md-5">
+                                    <button type="submit" class="btn btn-dark btn-custom btn-block m-t-30">
+                                        <i class="feather icon-filter mr-1"></i>
+                                        <span class="text-bold">Filter</span>
+                                    </button>
+                                    @if ($request->filter == true) 
+                                        <a href="{{ route('administration.league.index') }}" class="btn btn-link text-danger text-bold float-end float-right">
+                                            <i class="icon feather icon-x m-r-1"></i>
+                                            Clear
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
