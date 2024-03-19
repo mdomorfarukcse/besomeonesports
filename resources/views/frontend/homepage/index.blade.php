5798914e5343542c741b8bab80af07eb557ce3fe
Md Omor Faruk <mdomorfaruk.lu.cse@gmail.com>
2024-03-19 17:50:13 +0600

    video update

--- resources/views/frontend/homepage/index.blade.php
+++ resources/views/frontend/homepage/index.blade.php
@@ -43,7 +43,8 @@
             z-index: 999999;
         }
 
-        .img-container,.card-img {
+        .img-container,
+        .card-img {
             height: 450px;
             /* Fixed height for image containers */
             overflow: hidden;
@@ -62,39 +63,41 @@
         .about-sec-home .row>* {
             padding-right: 0px !important;
         }
+
         @media (max-width: 767px) {
-		.carousel-inner .carousel-item > div {
-			display: none;
-		}
-		.carousel-inner .carousel-item > div:first-child {
-			display: block;
-		}
-	}
+            .carousel-inner .carousel-item>div {
+                display: none;
+            }
 
-	.carousel-inner .carousel-item.active,
-	.carousel-inner .carousel-item-next,
-	.carousel-inner .carousel-item-prev {
-		display: flex;
-	}
+            .carousel-inner .carousel-item>div:first-child {
+                display: block;
+            }
+        }
 
-	/* medium and up screens */
-	@media (min-width: 768px) {
+        .carousel-inner .carousel-item.active,
+        .carousel-inner .carousel-item-next,
+        .carousel-inner .carousel-item-prev {
+            display: flex;
+        }
 
-		.carousel-inner .carousel-item-end.active,
-		.carousel-inner .carousel-item-next {
-			transform: translateX(25%);
-		}
+        /* medium and up screens */
+        @media (min-width: 768px) {
 
-		.carousel-inner .carousel-item-start.active, 
-		.carousel-inner .carousel-item-prev {
-			transform: translateX(-25%);
-		}
-	}
+            .carousel-inner .carousel-item-end.active,
+            .carousel-inner .carousel-item-next {
+                transform: translateX(25%);
+            }
 
-	.carousel-inner .carousel-item-end,
-	.carousel-inner .carousel-item-start { 
-		transform: translateX(0);
-	}
+            .carousel-inner .carousel-item-start.active,
+            .carousel-inner .carousel-item-prev {
+                transform: translateX(-25%);
+            }
+        }
+
+        .carousel-inner .carousel-item-end,
+        .carousel-inner .carousel-item-start {
+            transform: translateX(0);
+        }
     </style>
 @endsection
 
@@ -111,7 +114,8 @@
                             <div class="col-md-4">
                                 <div class="card">
                                     <div class="card-img">
-                                        <img src="{{ asset('frontend/images/402858967_838498248279910_284723558559234273_n-min.jpg') }}" alt="pic2" />
+                                        <img src="{{ asset('frontend/images/402858967_838498248279910_284723558559234273_n-min.jpg') }}"
+                                            alt="pic2" />
                                     </div>
                                 </div>
                             </div>
@@ -120,7 +124,7 @@
                             <div class="col-md-4">
                                 <div class="card">
                                     <div class="card-img">
-                                        <img src="{{ asset('frontend/images/about1.jpg') }}" alt="pic2" />                                   
+                                        <img src="{{ asset('frontend/images/about1.jpg') }}" alt="pic2" />
                                     </div>
                                 </div>
                             </div>
@@ -129,7 +133,8 @@
                             <div class="col-md-4">
                                 <div class="card">
                                     <div class="card-img">
-                                        <img src="{{ asset('frontend/images/BeSomone Sports Basketball  Summer 2023 All Star Game 08-19-23 (298) (1)-min.jpg') }}" alt="pic2" />                                    
+                                        <img src="{{ asset('frontend/images/BeSomone Sports Basketball  Summer 2023 All Star Game 08-19-23 (298) (1)-min.jpg') }}"
+                                            alt="pic2" />
                                     </div>
                                 </div>
                             </div>
@@ -138,7 +143,8 @@
                             <div class="col-md-4">
                                 <div class="card">
                                     <div class="card-img">
-                                        <img src="{{ asset('frontend/images/IMG_20231109_224546_208-min.jpg') }}" alt="pic2" />   
+                                        <img src="{{ asset('frontend/images/IMG_20231109_224546_208-min.jpg') }}"
+                                            alt="pic2" />
                                     </div>
                                 </div>
                             </div>
@@ -147,7 +153,8 @@
                             <div class="col-md-4">
                                 <div class="card">
                                     <div class="card-img">
-                                        <img src="{{ asset('frontend/images/Photo Jan 04 2024, 4 51 31 PM-min.jpg') }}" alt="pic2" />                                    
+                                        <img src="{{ asset('frontend/images/Photo Jan 04 2024, 4 51 31 PM-min.jpg') }}"
+                                            alt="pic2" />
                                     </div>
                                 </div>
                             </div>
@@ -156,20 +163,22 @@
                             <div class="col-md-4">
                                 <div class="card">
                                     <div class="card-img">
-                                        <img src="{{ asset('frontend/images/besomeonesport_about.jpg') }}" alt="pic2" /> 
+                                        <img src="{{ asset('frontend/images/besomeonesport_about.jpg') }}" alt="pic2" />
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                    <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button"
+                        data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     </a>
-                    <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
+                    <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button"
+                        data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     </a>
                 </div>
-            </div>		
+            </div>
         </div>
 
         {{-- <div class="gb-roguch">
@@ -210,16 +219,13 @@
                             around. </h5>
                     </div>
                     <div class="col-md-4 img-container">
-                        <img src="{{ asset('frontend/images/ZzZ_jIQM-min.jpeg') }}"
-                            alt="pic2" />
+                        <img src="{{ asset('frontend/images/ZzZ_jIQM-min.jpeg') }}" alt="pic2" />
                     </div>
                     <div class="col-md-4 img-container">
-                        <img src="{{ asset('frontend/images/Photo Jan 04 2024, 4 51 31 PM-min.jpg') }}"
-                            alt="pic2" />
+                        <img src="{{ asset('frontend/images/Photo Jan 04 2024, 4 51 31 PM-min.jpg') }}" alt="pic2" />
                     </div>
                     <div class="col-md-4 img-container">
-                        <img src="{{ asset('frontend/images/010_bso_mvp_vball_2023-min.jpg') }}"
-                            alt="pic2" />
+                        <img src="{{ asset('frontend/images/010_bso_mvp_vball_2023-min.jpg') }}" alt="pic2" />
                     </div>
                     <div class="col-md-12 mt-3 mb-3">
                         <p>Be Someone Sports was created to upgrade every aspect of youth sports. Our unique league provides
@@ -234,8 +240,7 @@
                             alt="pic2" />
                     </div>
                     <div class="col-md-4 img-container">
-                        <img src="{{ asset('frontend/images/027_bso_mvp_vball_2023 (1)-min.jpg') }}"
-                            alt="pic2" />
+                        <img src="{{ asset('frontend/images/027_bso_mvp_vball_2023 (1)-min.jpg') }}" alt="pic2" />
                     </div>
                     <div class="col-md-4 img-container">
                         <img src="{{ asset('frontend/images/Photo Jan 04 2024, 5 00 03 PM (1)-min.jpg') }}"
@@ -253,16 +258,13 @@
                         </p>
                     </div>
                     <div class="col-md-4 img-container">
-                        <img src="{{ asset('frontend/images/Photo Jan 04 2024, 4 56 33 PM-min.jpg') }}"
-                            alt="pic2" />
+                        <img src="{{ asset('frontend/images/Photo Jan 04 2024, 4 56 33 PM-min.jpg') }}" alt="pic2" />
                     </div>
                     <div class="col-md-4 img-container">
-                        <img src="{{ asset('frontend/images/IMG_20231109_224546_208-min.jpg') }}"
-                            alt="pic2" />
+                        <img src="{{ asset('frontend/images/IMG_20231109_224546_208-min.jpg') }}" alt="pic2" />
                     </div>
                     <div class="col-md-4 img-container">
-                        <img src="{{ asset('frontend/images/wajI0n-d-min.jpeg') }}"
-                            alt="pic2" />
+                        <img src="{{ asset('frontend/images/wajI0n-d-min.jpeg') }}" alt="pic2" />
                     </div>
                     <div class="col-md-12 mt-3 mb-3 text-center">
                         <h1>Be Someone who makes you proud. Be Someone Sports.</h1>
@@ -272,8 +274,7 @@
                             alt="pic2" />
                     </div>
                     <div class="col-md-3 img-container">
-                        <img src="{{ asset('frontend/images/003_BSS_All-Stars-min.jpg') }}"
-                            alt="pic2" />
+                        <img src="{{ asset('frontend/images/003_BSS_All-Stars-min.jpg') }}" alt="pic2" />
                     </div>
                     <div class="col-md-5 img-container">
                         <img src="{{ asset('frontend/images/402858967_838498248279910_284723558559234273_n-min.jpg') }}"
@@ -316,19 +317,23 @@
                             <div class="carousel-inner">
                                 @foreach ($upcomingLeagues as $key => $league)
                                     <div class="carousel-item @if ($key == 0) active @endif">
-                                        <img src="{{ show_image($league->logo) }}" class="d-block w-100" alt="..." />
+                                        <img src="{{ show_image($league->logo) }}" class="d-block w-100"
+                                            alt="..." />
                                         <div class="carousel-caption d-none d-md-block">
                                             <h6>{{ $league->name }}</h6>
-                                            <p>Winning and losing are a part of everyday life. This is not the everyone wins league.</p>
+                                            <p>Winning and losing are a part of everyday life. This is not the everyone wins
+                                                league.</p>
                                             <a href="{{ route('administration.league.registration', ['league' => $league]) }}"
-                                                class="btn btn-warning btn-outline-custom fw-bolder"> <i class="fas fa-tags"></i> Register</a>
+                                                class="btn btn-warning btn-outline-custom fw-bolder"> <i
+                                                    class="fas fa-tags"></i> Register</a>
                                             <a href="{{ route('frontend.league.show', ['league' => $league]) }}"
-                                                class="btn btn-info btn-outline-custom fw-bolder"> <i class="fas fa-tags"></i> Details</a>
+                                                class="btn btn-info btn-outline-custom fw-bolder"> <i
+                                                    class="fas fa-tags"></i> Details</a>
                                         </div>
                                     </div>
                                 @endforeach
                             </div>
-                            @if(count($upcomingLeagues) > 1)
+                            @if (count($upcomingLeagues) > 1)
                                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselleague"
                                     data-bs-slide="prev">
                                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -340,7 +345,7 @@
                                     <span class="visually-hidden">Next</span>
                                 </button>
                             @endif
-                        </div>  
+                        </div>
                     @else
                         <h5 class="alert alert-info">No Data</h5>
                     @endif
@@ -454,24 +459,25 @@
         </div>
 
         <!-- Start row -->
-    <section class="float-start w-100 sub-headh-bask body-part pt-0 mb-50">
-        <div class="about-page-main comon-sub-page-main d-inline-block w-100">
-            <div class="about-club-top">
-                <div class="container">
-                    <div class="row ">
-                        <div class="text-center mb-50">
-                            <h5 class="samll-sub mb-1 mt-0">Our Story</h5>
-                            <h2 class="comon-heading m-0">Reviews</h2>
-                        </div>
-                        <div class="col-md-12">
-                            <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
-                            <div class="elfsight-app-4d1d8a6a-9211-40dd-90d4-01d3e4f24ef5" data-elfsight-app-lazy></div>
+        <section class="float-start w-100 sub-headh-bask body-part pt-0 mb-50">
+            <div class="about-page-main comon-sub-page-main d-inline-block w-100">
+                <div class="about-club-top">
+                    <div class="container">
+                        <div class="row ">
+                            <div class="text-center mb-50">
+                                <h5 class="samll-sub mb-1 mt-0">Our Story</h5>
+                                <h2 class="comon-heading m-0">Reviews</h2>
+                            </div>
+                            <div class="col-md-12">
+                                <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+                                <div class="elfsight-app-4d1d8a6a-9211-40dd-90d4-01d3e4f24ef5" data-elfsight-app-lazy>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
         {{-- <div class="mediasection d-inline-block w-100">
             <div class="container">
@@ -510,8 +516,10 @@
                 <div class="row row-cols-2 row-cols-lg-4 mt-0 g-4 mt-3">
                     @foreach ($videos as $key => $video)
                         <div class="col-md-6">
-                            <iframe width="100%" height="345" src="{{ $video->youtubeurl }}">
-                            </iframe>
+                            <iframe width="100%" height="345" src="{{ $video->youtubeurl }}" frameborder="0"
+                                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen></iframe>
+
                         </div>
                     @endforeach
                 </div>
@@ -577,7 +585,8 @@
                                 <div class="ban-ati-com ads-all-list">
                                     <a href="/contact">
                                         <span>Ad</span>
-                                        <img src="{{ asset('frontend/images/register.png') }}" height="100" alt="" />
+                                        <img src="{{ asset('frontend/images/register.png') }}" height="100"
+                                            alt="" />
                                     </a>
                                 </div>
                             </div>
@@ -585,12 +594,13 @@
                                 <div class="ban-ati-com ads-all-list">
                                     <a href="">
                                         <span>Ad</span>
-                                        <img src="{{ asset('frontend/images/ad.png') }}" height="100" alt="" />
+                                        <img src="{{ asset('frontend/images/ad.png') }}" height="100"
+                                            alt="" />
                                     </a>
                                 </div>
                             </div>
                         </div>
-                    </div> 
+                    </div>
                 </div>
             </div>
         </div>
@@ -653,23 +663,22 @@
         $(window).on("load", function() {
             // $("#leaguemodal").modal("show");
         });
-        
+
         let items = document.querySelectorAll("#sliderCarousel .carousel-item");
 
         items.forEach((el) => {
-        const minPerSlide = 3;
-        let next = el.nextElementSibling;
-        for (var i = 1; i < minPerSlide; i++) {
-            if (!next) {
-            // wrap carousel by using first child
-            next = items[0];
+            const minPerSlide = 3;
+            let next = el.nextElementSibling;
+            for (var i = 1; i < minPerSlide; i++) {
+                if (!next) {
+                    // wrap carousel by using first child
+                    next = items[0];
+                }
+                let cloneChild = next.cloneNode(true);
+                el.appendChild(cloneChild.children[0]);
+                next = next.nextElementSibling;
             }
-            let cloneChild = next.cloneNode(true);
-            el.appendChild(cloneChild.children[0]);
-            next = next.nextElementSibling;
-        }
         });
-
     </script>
 
 @endsection
