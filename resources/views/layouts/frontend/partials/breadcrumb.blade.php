36257a5e05faeb6aa48b53848515bb3ad70eeeac
Md Omor Faruk <mdomorfaruk.lu.cse@gmail.com>
2024-05-15 13:15:13 +0600

    Done 81,88, 94 changes

--- resources/views/layouts/frontend/partials/breadcrumb.blade.php
+++ resources/views/layouts/frontend/partials/breadcrumb.blade.php
@@ -1,10 +1,23 @@
 <section class="banner-part sub-main-banner float-start w-100 breadcrumb-section">
+
     <div class="baner-imghi">
-        <img src="{{ asset('frontend/images/banner2.jpg') }}" alt="sub-banner" />
+        {{request()->path()}}
+        @switch(request()->path())
+            @case('league')
+                <img src="{{ asset('frontend/images/breadcrumb_banners/breadcrumb_banner3.png') }}" alt="sub-banner" />
+                @break
+
+            @case('our-team')
+                <img src="{{ asset('frontend/images/breadcrumb_banners/breadcrumb_banner6.png') }}" alt="sub-banner" />
+                @break
+
+            @default
+                <img src="{{ asset('frontend/images/breadcrumb_banners/breadcrumb_banner2.png') }}" alt="default-banner" />
+        @endswitch
     </div>
     <style>
         .sub-main-banner .baner-imghi img {
-            object-position: top !important;
+            object-position: center !important;
         }
     </style>
     <div class="sub-banner">
