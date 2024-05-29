c952842b880405c17723ea243bc4a156332a504d
Md Omor Faruk <mdomorfaruk.lu.cse@gmail.com>
2024-05-29 20:38:15 +0600

    breadcrumb image logic update

--- resources/views/layouts/frontend/partials/breadcrumb.blade.php
+++ resources/views/layouts/frontend/partials/breadcrumb.blade.php
@@ -3,44 +3,46 @@
     <div class="baner-imghi">
         @switch(request()->path())
             @case('about')
+            @case('mission')
+            @case('our-team')
+            @case('testimonials')
+            @case('app-info')
                 <img src="{{ asset('frontend/images/breadcrumb_banners/about_us.png') }}" alt="sub-banner" />
                 @break
 
             @case('league')
-                <img src="{{ asset('frontend/images/breadcrumb_banners/leagues.png') }}" alt="sub-banner" />
-                @break
-            
             @case('coach/create')
-                <img src="{{ asset('frontend/images/breadcrumb_banners/leagues.png') }}" alt="sub-banner" />
-                @break
-            
             @case('stats')
                 <img src="{{ asset('frontend/images/breadcrumb_banners/leagues.png') }}" alt="sub-banner" />
                 @break
-                          
+
             @case('gallery')
                 <img src="{{ asset('frontend/images/breadcrumb_banners/gallery.png') }}" alt="sub-banner" />
                 @break
 
             @case('sponsors')
+            @case('advertise-with-us')
+            @case('partners')
                 <img src="{{ asset('frontend/images/breadcrumb_banners/our_sponsors.png') }}" alt="sub-banner" />
                 @break
 
             @case('blog')
+            @case('media-inquiries')
                 <img src="{{ asset('frontend/images/breadcrumb_banners/news.jpg') }}" alt="sub-banner" />
                 @break
-                
+
             @case('contact')
                 <img src="{{ asset('frontend/images/breadcrumb_banners/contact.png') }}" alt="sub-banner" />
                 @break
-                
+
             @case('shop')
                 <img src="{{ asset('frontend/images/breadcrumb_banners/shop.png') }}" alt="sub-banner" />
                 @break
-            
+
             @default
                 <img src="{{ asset('frontend/images/h1-rev-img-2.png') }}" alt="default-banner" />
         @endswitch
+
     </div>
     <style>
         .sub-main-banner .baner-imghi img {
