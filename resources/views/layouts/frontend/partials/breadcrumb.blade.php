3880d1b35c9de0843300b5251013f17532524d6a
Md Omor Faruk <mdomorfaruk.lu.cse@gmail.com>
2024-05-29 18:57:08 +0600

    breadcrumb_banners update

--- resources/views/layouts/frontend/partials/breadcrumb.blade.php
+++ resources/views/layouts/frontend/partials/breadcrumb.blade.php
@@ -1,18 +1,37 @@
 <section class="banner-part sub-main-banner float-start w-100 breadcrumb-section">
 
     <div class="baner-imghi">
-        {{request()->path()}}
         @switch(request()->path())
+            @case('about')
+                <img src="{{ asset('frontend/images/breadcrumb_banners/about_us.png') }}" alt="sub-banner" />
+                @break
+
             @case('league')
-                <img src="{{ asset('frontend/images/breadcrumb_banners/breadcrumb_banner3.png') }}" alt="sub-banner" />
+                <img src="{{ asset('frontend/images/breadcrumb_banners/leagues.png') }}" alt="sub-banner" />
                 @break
 
-            @case('our-team')
-                <img src="{{ asset('frontend/images/breadcrumb_banners/breadcrumb_banner6.png') }}" alt="sub-banner" />
+            @case('gallery')
+                <img src="{{ asset('frontend/images/breadcrumb_banners/gallery.png') }}" alt="sub-banner" />
                 @break
 
+            @case('sponsors')
+                <img src="{{ asset('frontend/images/breadcrumb_banners/our_sponsors.png') }}" alt="sub-banner" />
+                @break
+
+            @case('blog')
+                <img src="{{ asset('frontend/images/breadcrumb_banners/news.jpg') }}" alt="sub-banner" />
+                @break
+                
+            @case('contact')
+                <img src="{{ asset('frontend/images/breadcrumb_banners/contact.png') }}" alt="sub-banner" />
+                @break
+                
+            @case('shop')
+                <img src="{{ asset('frontend/images/breadcrumb_banners/shop.png') }}" alt="sub-banner" />
+                @break
+            
             @default
-                <img src="{{ asset('frontend/images/breadcrumb_banners/breadcrumb_banner2.png') }}" alt="default-banner" />
+                <img src="{{ asset('frontend/images/breadcrumb_banners/about_us.png') }}" alt="default-banner" />
         @endswitch
     </div>
     <style>
